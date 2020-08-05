7d4bd45da3f4b70bf0be08bf8f296fe1432502ff
Gabriel Henrique <gabrielhenrique@MacBook-Air-de-Gabriel.local>
2020-08-05 00:00:23 -0300

    project ready to production

---
+++ app/helpers/exchanges_helper.rb
@@ -0,0 +1,11 @@
+module ExchangesHelper
+    
+    def currency_list
+        [
+            'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 
+            'GBP', 'HKD', 'HKR', 'HRF', 'HUR', 'IDR', 'ILS', 'INR', 'JPY', 
+            'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 
+            'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR'
+        ]
+    end
+end
