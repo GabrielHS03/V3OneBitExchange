7f891845cbc9682e4d732934dee9a2461186f658
Gabriel Henrique <gabrielhenrique@MacBook-Air-de-Gabriel.local>
2020-08-11 15:23:45 -0300

    Includes BTC in conversion

--- app/helpers/exchanges_helper.rb
+++ app/helpers/exchanges_helper.rb
@@ -2,7 +2,7 @@ module ExchangesHelper
     
     def currency_list
         [
-            'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 
+            'AUD', 'BGN', 'BRL', 'BTC','CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 
             'GBP', 'HKD', 'HKR', 'HRF', 'HUR', 'IDR', 'ILS', 'INR', 'JPY', 
             'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 
             'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR'
